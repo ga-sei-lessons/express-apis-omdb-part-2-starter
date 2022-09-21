681bf88c1fa1b92764fcd2758676367d3c507293
KhakiSuitValentino <iamschoolcraft@gmail.com>
2022-09-21 12:08:23 -0400

    deliverable

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
 const axios = require('axios')
+const db = require('./models')
 
 // Sets EJS as the view engine
 app.set('view engine', 'ejs');
@@ -24,6 +25,7 @@ app.get('/', function(req, res) {
 app.get('/results', (req, res) => {
   axios.get(`http://www.omdbapi.com/?s=${req.query.movieSearch}&apikey=${process.env.OMDB_API_KEY}`)
     .then(response => {
+      console.log(response.data);
       res.render('results.ejs', { movies: response.data.Search })
     })
     .catch(console.log)
@@ -36,6 +38,30 @@ app.get('/details/:id', (req, res) => {
       res.render('detail.ejs', { movie: response.data })
     })
     .catch(console.log)
+})
+
+//FAVES ROUTES:
+// GET /faves : READ all faves and display to user
+app.get('/faves', async (req, res) => {
+  try {
+    const allFaves = await db.fave.findAll()
+    res.render('faves.ejs', {allFaves});
+
+  } catch(err) {
+    console.log(err);
+    res.send('server error');
+  }
+})
+
+//POST /faves : CREATE new fave & redirect to /faves to display user faves
+app.post('/faves', async (req, res) => {
+    try {
+      await db.fave.create(req.body);
+      res.redirect('/faves');
+  } catch(err) {
+      res.send('server error');
+      console.log(err);
+  }
 })
 
 app.listen(process.env.PORT || 3000);
